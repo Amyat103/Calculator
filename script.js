7376262d8e02573c4e72aabb68e08aeba42b5408
Amyat103 <zhoudavid7312@gmail.com>
2024-02-19 00:20:31 -0500

    fix javascript, add listener to all buttons

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const numPad = document.querySelector(".num_pad");
+const screen = document.querySelector(".screen");
 
+let input = "";
+
+// load the numbers buttons
 window.onload = function () {
     for (let i = 1; i < 10; i++) {
         const eachNum = document.createElement("button");
         eachNum.textContent = i;
+        eachNum.setAttribute("value", i);
+        eachNum.setAttribute("class", "button");
         numPad.append(eachNum);
+        // eachNum.addEventListener("click" () {
+        //     input += this.value;
+        // })
     }
     const zeroNum = document.createElement("button");
     zeroNum.textContent = 0;
+    zeroNum.setAttribute("value", "10");
+    zeroNum.setAttribute("class", "button");
     numPad.append(zeroNum);
 }
+
+
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach(function(button) {
+    button.addEventListener("click", something)
+});
+
+function calculate() {
+
+}
+
 
