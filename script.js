40eefc7e9e201737dea62ffe15d93c7d2446479e
Amyat103 <zhoudavid7312@gmail.com>
2024-02-18 23:33:20 -0500

    change div to buttons, fix js to add buttons

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const numPad = document.querySelector(".num_pad");
 
-const add_numpad = function () {
+window.onload = function () {
     for (let i = 1; i < 10; i++) {
-        const eachNum = document.createElement("div");
+        const eachNum = document.createElement("button");
         eachNum.textContent = i;
         numPad.append(eachNum);
     }
+    const zeroNum = document.createElement("button");
+    zeroNum.textContent = 0;
+    numPad.append(zeroNum);
 }
 
